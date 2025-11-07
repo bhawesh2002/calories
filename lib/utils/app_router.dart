6998c7fb3246eb59af3d1439311e8193f294c303
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-07 15:03:51 +0530

    feat: register routes in AppRoutes

---
+++ lib/utils/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:calories/home/views/home.dart';
+import 'package:calories/recipe/views/recipe_details_page.dart';
+import 'package:calories/recipe/views/recipes_page.dart';
+import 'package:calories/splash/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const splash = "/";
+  static const home = '/home';
+  static const recipes = '/recipes';
+  static const recipeDetails = '$recipes/details';
+
+  static final Map<String, WidgetBuilder> routesMap = <String, WidgetBuilder>{
+    splash: (context) => SplashScreen(),
+    home: (context) => Home(),
+    recipes: (context) => RecipesPage(),
+    recipeDetails: (context) => RecipeDetailsPage(),
+  };
+}
