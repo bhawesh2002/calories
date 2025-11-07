4c48462f5452d9965237d7c5b877af5e8374c194
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-07 14:34:46 +0530

    update material app with supportedLocales and delegates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:calories/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +12,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'SFProDisplay'),
+      locale: Locale('en'),
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            spacing: 16,
+            children: [
+              Text(
+                'Let\'s start your\nhealthy day',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: -0.35,
+                ),
+              ),
+              SFIcon(SFIcons.sf_heart_fill),
+            ],
+          ),
         ),
       ),
     );
