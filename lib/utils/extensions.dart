a85b98ec6230f125318ab600d811b763d59c9c4e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-08 11:39:31 +0530

    rename AppLoc to AppLocale

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,7 +1,7 @@
 import 'package:calories/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
-extension AppLoc on State {
+extension AppLocale on State {
   AppLocalizations get appLocale => AppLocalizations.of(context)!;
 }
 
