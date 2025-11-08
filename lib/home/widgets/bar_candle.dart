72f8bdb863a803b5caa9bc31bd44ae9bedcc23f4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-08 19:06:37 +0530

    feat: add animation to candle

--- lib/home/widgets/bar_candle.dart
+++ lib/home/widgets/bar_candle.dart
@@ -27,51 +27,118 @@ class BarCandle extends StatefulWidget {
   State<BarCandle> createState() => _BarCandleState();
 }
 
-class _BarCandleState extends State<BarCandle> {
+class _BarCandleState extends State<BarCandle>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  Animation<Size>? sizeAnim;
+  Animation<double>? completedAnim;
+
+  BoxConstraints? _constraints;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    setSizeAnim();
+
+    super.initState();
+  }
+
+  void setSizeAnim() async {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_constraints != null) {
+        sizeAnim ??=
+            Tween<Size>(
+              begin: Size(_constraints!.maxWidth, 0),
+              end: Size(_constraints!.maxWidth, _constraints!.maxHeight),
+            ).animate(
+              CurvedAnimation(
+                parent: animationController,
+                curve: Interval(0.0, 1.0, curve: Curves.easeInOut),
+              ),
+            );
+        completedAnim ??= Tween<double>(begin: 0, end: widget.completed)
+            .animate(
+              CurvedAnimation(
+                parent: animationController,
+                curve: Interval(0.65, 1.0, curve: Curves.easeInOut),
+              ),
+            );
+        animationController.forward();
+      } else {
+        setSizeAnim();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        RichText(
-          text: TextSpan(
-            text: widget.title,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-              color: Colors.black,
-            ),
-            children: [
-              TextSpan(
-                text: widget.subtitle,
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w300,
-                  color: Colors.grey.shade600,
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (BuildContext context, Widget? child) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedOpacity(
+              curve: Curves.ease,
+              duration: const Duration(milliseconds: 300),
+              opacity: animationController.isCompleted ? 1 : 0,
+              child: RichText(
+                text: TextSpan(
+                  text: widget.title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: widget.subtitle,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-        AppSpacing.v4,
-        Expanded(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return CustomPaint(
-                size: Size(constraints.maxHeight, constraints.maxWidth),
-                painter: BarCandlePainter(
-                  completed: widget.completed,
-                  spacing: widget.spacing,
-                  angle: widget.angle,
-                  color: widget.color,
-                  lineWidth: widget.lineWidth,
-                  radius: widget.radius,
-                ),
-              );
-            },
-          ),
-        ),
-      ],
+            ),
+            AppSpacing.v6,
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  _constraints = constraints;
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: GestureDetector(
+                      onTap: () {
+                        animationController.forward(from: 0.0);
+                      },
+                      child: SizedBox(
+                        width: constraints.maxWidth,
+                        height: sizeAnim?.value.height ?? 0,
+                        child: CustomPaint(
+                          painter: BarCandlePainter(
+                            completed: widget.completed,
+                            spacing: widget.spacing,
+                            angle: widget.angle,
+                            color: widget.color,
+                            lineWidth: widget.lineWidth,
+                            radius: widget.radius,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
