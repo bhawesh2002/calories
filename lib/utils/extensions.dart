879f8e9f77fb4103e03669e82222ddf60fb5d3af
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-08 11:39:12 +0530

    feat: add extensions for AppLocale, DeviceDimensions and DevicePadding

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,32 @@
+import 'package:calories/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+extension AppLoc on State {
+  AppLocalizations get appLocale => AppLocalizations.of(context)!;
+}
+
+extension DeviceDimensions on BuildContext {
+  double get deviceWt => MediaQuery.sizeOf(this).width;
+  double get deviceHt => MediaQuery.sizeOf(this).height;
+  double get viewPaddingBottom => MediaQuery.viewPaddingOf(this).bottom;
+  double get viewPaddingTop => MediaQuery.viewPaddingOf(this).top;
+  double get viewInsetsBottom => MediaQuery.viewInsetsOf(this).bottom;
+  EdgeInsets get deviceInsets =>
+      EdgeInsets.only(bottom: viewInsetsBottom, top: viewPaddingTop);
+}
+
+extension DevicePadding on BuildContext {
+  Padding devicePadding(
+    BuildContext context, {
+    required Column child,
+    double bottom = 12,
+    double top = 12,
+    double left = 16,
+    double right = 16,
+  }) => Padding(
+    padding:
+        deviceInsets +
+        EdgeInsets.only(bottom: bottom, top: top, left: left, right: right),
+    child: child,
+  );
+}
