3a2d7698e9d0384cc75c5c519d53f01a57d1451e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-08 11:37:55 +0530

    feat: add AppSpacing class with spacing values

---
+++ lib/utils/app_spacing.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/widgets.dart';
+
+class AppSpacing {
+  static SizedBox vSpace(double height) => SizedBox(height: height);
+  static SizedBox hSpace(double width) => SizedBox(width: width);
+
+  static Expanded get expandedSpace => Expanded(child: SizedBox());
+
+  static const SizedBox v2 = SizedBox(height: 2);
+  static const SizedBox v4 = SizedBox(height: 4);
+  static const SizedBox v6 = SizedBox(height: 6);
+  static const SizedBox v8 = SizedBox(height: 8);
+  static const SizedBox v10 = SizedBox(height: 10);
+  static const SizedBox v12 = SizedBox(height: 12);
+  static const SizedBox v14 = SizedBox(height: 14);
+  static const SizedBox v16 = SizedBox(height: 16);
+  static const SizedBox v18 = SizedBox(height: 18);
+  static const SizedBox v20 = SizedBox(height: 20);
+  static const SizedBox v22 = SizedBox(height: 22);
+  static const SizedBox v24 = SizedBox(height: 24);
+  static const SizedBox v26 = SizedBox(height: 26);
+  static const SizedBox v28 = SizedBox(height: 28);
+  static const SizedBox v30 = SizedBox(height: 30);
+  static const SizedBox v32 = SizedBox(height: 32);
+  static const SizedBox v34 = SizedBox(height: 34);
+  static const SizedBox v36 = SizedBox(height: 36);
+  static const SizedBox v38 = SizedBox(height: 38);
+  static const SizedBox v40 = SizedBox(height: 40);
+
+  static const SizedBox h2 = SizedBox(width: 2);
+  static const SizedBox h4 = SizedBox(width: 4);
+  static const SizedBox h6 = SizedBox(width: 6);
+  static const SizedBox h8 = SizedBox(width: 8);
+  static const SizedBox h10 = SizedBox(width: 10);
+  static const SizedBox h12 = SizedBox(width: 12);
+  static const SizedBox h14 = SizedBox(width: 14);
+  static const SizedBox h16 = SizedBox(width: 16);
+  static const SizedBox h18 = SizedBox(width: 18);
+  static const SizedBox h20 = SizedBox(width: 20);
+  static const SizedBox h22 = SizedBox(width: 22);
+  static const SizedBox h24 = SizedBox(width: 24);
+  static const SizedBox h26 = SizedBox(width: 26);
+  static const SizedBox h28 = SizedBox(width: 28);
+  static const SizedBox h30 = SizedBox(width: 30);
+  static const SizedBox h32 = SizedBox(width: 32);
+  static const SizedBox h34 = SizedBox(width: 34);
+  static const SizedBox h36 = SizedBox(width: 36);
+  static const SizedBox h38 = SizedBox(width: 38);
+  static const SizedBox h40 = SizedBox(width: 40);
+}
