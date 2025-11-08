1ad10d922b8789ca5db303660a6dc72e1443af30
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-08 20:38:26 +0530

    feat: register asset path in app assets

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String recipeDetails = 'assets/images/recipe_details.jpg';
+  static const String recipeSuggestion1 =
+      'assets/images/recipe_suggestion1.jpg';
+  static const String recipeSuggestion2 =
+      'assets/images/recipe_suggestion2.png';
+}
