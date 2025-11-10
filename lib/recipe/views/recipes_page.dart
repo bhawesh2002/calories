1d19dbd73776f12cb6390c625a8a5512e37b4f14
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-10 08:21:10 +0530

    feat: add ui code for recipies page

--- lib/recipe/views/recipes_page.dart
+++ lib/recipe/views/recipes_page.dart
@@ -1,4 +1,10 @@
+import 'package:calories/recipe/widgets/circle_guage.dart';
+import 'package:calories/clippers/custom_image_clipper.dart';
+import 'package:calories/utils/app_assets.dart';
+import 'package:calories/utils/app_spacing.dart';
+import 'package:calories/utils/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
 
 class RecipesPage extends StatefulWidget {
   const RecipesPage({super.key});
@@ -10,6 +16,113 @@ class RecipesPage extends StatefulWidget {
 class _RecipesPageState extends State<RecipesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: context.devicePadding(
+        context,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: SFIcon(SFIcons.sf_chevron_left, fontSize: 22),
+            ),
+            AppSpacing.v8,
+            SizedBox(
+              height: context.deviceHt * 0.45,
+              child: ClipPath(
+                clipper: CustomImageClipper.v1(),
+                child: Image.asset(AppAssets.recipeDetails, fit: BoxFit.fill),
+              ),
+            ),
+            AppSpacing.v12,
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      appLocale.lazyOatmeal,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      appLocale.recipeMeta,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                SFIcon(SFIcons.sf_heart_fill, color: Colors.black),
+              ],
+            ),
+
+            AppSpacing.v18,
+            Column(
+              spacing: 10,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  appLocale.descriptionTitle,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  appLocale.lazyOatmealDescription,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+              ],
+            ),
+            AppSpacing.v12,
+
+            Text(
+              appLocale.nutritionalValue,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            AppSpacing.v20,
+
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ...List.generate(3, (index) {
+                    double completionValue() {
+                      if (index == 0) {
+                        return 0.6;
+                      } else if (index == 1) {
+                        return 0.35;
+                      } else {
+                        return 0.22;
+                      }
+                    }
+
+                    return Flexible(
+                      child: CircleGuage(
+                        completed: completionValue(),
+                        title: "380 g",
+                        subtitle: "Fat",
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+            AppSpacing.v18,
+          ],
+        ),
+      ),
+    );
   }
 }
