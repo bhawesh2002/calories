ab62db30b8fd508f3ed28a9366fd8a3a642d01a9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-11 18:45:33 +0530

    feat: optimize code for  triple circle clipper and add hemisphere distance property

--- lib/clippers/triple_circle_clipper.dart
+++ lib/clippers/triple_circle_clipper.dart
@@ -2,36 +2,33 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 class TripleCircleClipper extends CustomClipper<Path> {
+  final double hemisphereDistance;
+  TripleCircleClipper({this.hemisphereDistance = 0.0});
+
   @override
   Path getClip(Size size) {
     final path = Path();
+    double arcDistance = 0;
+    double rectWidth = size.width;
+    for (var i = 0; i < 3; i++) {
+      path.addArc(
+        Rect.fromLTWH(
+          arcDistance + hemisphereDistance,
+          0,
+          rectWidth,
+          size.height,
+        ),
+        -math.pi / 2,
+        -math.pi,
+      );
+      arcDistance += (rectWidth / 2) + hemisphereDistance;
+      rectWidth = (rectWidth / 2) * 1.23;
+    }
 
-    path.addArc(
-      Rect.fromLTRB(0, 0, size.width, size.height),
-      -math.pi / 2,
-      -math.pi,
-    );
-    path.addArc(
-      Rect.fromPoints(
-        Offset(size.width * 0.5, 0),
-        Offset(size.width * 1.1, size.height),
-      ),
-      -math.pi / 2,
-      -math.pi,
-    );
-    path.addArc(
-      Rect.fromPoints(
-        Offset(size.width * 0.8, 0),
-        Offset(size.width * 1.2, size.height),
-      ),
-      -math.pi / 2,
-      -math.pi,
-    );
     return path;
   }
 
   @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
-  }
+  bool shouldReclip(covariant TripleCircleClipper oldClipper) => true;
+  // oldClipper.hemisphereDistance != hemisphereDistance;
 }
