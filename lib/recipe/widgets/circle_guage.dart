dd31af23e4702c9868a10b9478d5dc73ac091b00
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-11 18:46:00 +0530

    update animation duration for cirle gauge

--- lib/recipe/widgets/circle_guage.dart
+++ lib/recipe/widgets/circle_guage.dart
@@ -30,7 +30,7 @@ class _CircleGuageState extends State<CircleGuage>
   void initState() {
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 1),
     );
     startCompleteAnimation();
     super.initState();
