3a2a75045d2e1664b5c60b13de4d7e45ad405dc8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-11 16:10:49 +0530

    rename custom image clipper to triple circle clipper

--- lib/clippers/triple_circle_clipper.dart
+++ lib/clippers/triple_circle_clipper.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-enum CustomClipVarient { varient1, varient2, varient3 }
-
-class CustomImageClipper extends CustomClipper<Path> {
-  final CustomClipVarient clipVairent;
-  CustomImageClipper({required this.clipVairent});
-  factory CustomImageClipper.v1() =>
-      CustomImageClipper(clipVairent: CustomClipVarient.varient1);
-
-  Path _getPathVarient1(Size size) {
+class TripleCircleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
     final path = Path();
 
     path.addArc(
@@ -36,21 +30,8 @@ class CustomImageClipper extends CustomClipper<Path> {
     return path;
   }
 
-  Path _getPath(Size size) {
-    if (clipVairent == CustomClipVarient.varient1) {
-      return _getPathVarient1(size);
-    }
-    return Path();
-  }
-
-  @override
-  Path getClip(Size size) {
-    final Path path = _getPath(size);
-    return path;
-  }
-
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
+    return false;
   }
 }
