8e3677bf1d92b7f3dc94942deb73fa5ce206a045
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-13 21:41:01 +0530

    feat: create app typography with predefined text styles

---
+++ lib/utils/app_typography.dart
@@ -0,0 +1,60 @@
+import 'package:calories/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  TextStyle textXSmGreyed = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    letterSpacing: -0.5,
+    color: AppColors.grey,
+  );
+
+  TextStyle textSmGreyed = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    letterSpacing: -0.6,
+    color: AppColors.grey,
+  );
+
+  TextStyle textMd20Medium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    letterSpacing: -0.4,
+    color: AppColors.pureBlack,
+  );
+
+  TextStyle textMdMedium = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    letterSpacing: -0.4,
+    color: AppColors.pureBlack,
+  );
+
+  TextStyle textLMedium = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    letterSpacing: -0.6,
+    color: AppColors.pureBlack,
+  );
+
+  TextStyle textXLSemiBold = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    letterSpacing: -0.2,
+    color: AppColors.appBlack,
+  );
+
+  TextStyle textXXLSemiBold = TextStyle(
+    fontSize: 55,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    letterSpacing: -0.2,
+    color: AppColors.appBlack,
+  );
+}
