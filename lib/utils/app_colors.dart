9c9624d786a010387ffad34786e2234c1acc1cc5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-13 21:40:44 +0530

    feat: create app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appBlack = Color(0xff191919);
+  static const pureBlack = Color(0xff000000);
+  static const appWhite = Color(0xffF8F8F8);
+  static const pureWhite = Color(0xFFFFFFFF);
+
+  static const grey = Color(0xff828282);
+
+  static const green = Color(0xffADCB3D);
+  static const orange = Color(0xffFF7616);
+  static const pink = Color(0xffFFA7E7);
+}
