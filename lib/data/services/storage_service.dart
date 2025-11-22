dc70da4ded47555d59ac94d4398d84a37cc79d19
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-22 10:43:04 +0530

    feat: add StorageService for configuring shared preferences and hive

---
+++ lib/data/services/storage_service.dart
@@ -0,0 +1,59 @@
+import 'package:calories/data/models/permission_entry.dart';
+import 'package:calories/hive/hive_registrar.g.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum StorageBoxes {
+  permission('permissions', 'hive box for tracking app permissions'),
+  recepies('recipies', 'hive box for storing recipies'),
+  settings('settings', 'hive box for storing app settings');
+
+  final String boxName;
+  final String description;
+
+  const StorageBoxes(this.boxName, this.description);
+
+  static List<String> getBoxNames() =>
+      StorageBoxes.values.map((e) => e.toString()).toList();
+
+  @override
+  toString() => boxName;
+}
+
+class StorageService {
+  StorageService._();
+  static final StorageService instance = StorageService._();
+
+  late final SharedPreferences prefs;
+
+  bool _setupComplete = false;
+
+  static Future<void> setup() async {
+    if (instance._setupComplete) return;
+    await instance._setupSharedPrefs();
+    await instance._setupHive();
+    instance._setupComplete = true;
+  }
+
+  Future<void> _setupSharedPrefs() async {
+    if (_setupComplete) return;
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<void> _setupHive() async {
+    if (_setupComplete) return;
+    await Hive.initFlutter();
+    for (var box in StorageBoxes.getBoxNames()) {
+      await Hive.openBox<PermissionEntry>(box);
+    }
+    Hive.registerAdapters();
+  }
+
+  Box<T> getBox<T>(StorageBoxes box) {
+    return Hive.box<T>(box.boxName);
+  }
+
+  LazyBox<T> getLazyBox<T>(StorageBoxes box) {
+    return Hive.lazyBox<T>(box.boxName);
+  }
+}
