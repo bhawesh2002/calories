c892a75149049c44627954bfbb07ab130938166d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-24 13:04:56 +0530

    feat: request permissions in splash screen

--- lib/ui/splash/views/splash_screen.dart
+++ lib/ui/splash/views/splash_screen.dart
@@ -1,19 +1,26 @@
+import 'package:calories/data/providers/repository_provider.dart';
 import 'package:calories/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void didChangeDependencies() {
-    Future.delayed((const Duration(seconds: 2)), () {
-      if (mounted) Navigator.of(context).pushNamed(AppRoutes.home);
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await ref.read(permissionRepoProvider).accquireDeniedPermissions();
+
+      Future.delayed((const Duration(seconds: 1)), () {
+        if (mounted) Navigator.of(context).pushNamed(AppRoutes.home);
+      });
     });
+
     super.didChangeDependencies();
   }
 
