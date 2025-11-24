85b127c94aa19267c96bc3eba608124c7e103548
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-11-24 12:07:40 +0530

    feat: add permission service in service providers

---
+++ lib/data/providers/service_providers.dart
@@ -0,0 +1,4 @@
+import 'package:calories/data/services/permission_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final permissionService = Provider((ref) => PermissionService());
