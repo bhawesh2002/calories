dfcc28c8726b56e395bd25f161b3b16209759e7f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-12-07 09:58:01 +0530

    feat: disaply error message on screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 
 import 'package:calories/data/services/storage_service.dart';
 import 'package:calories/l10n/app_localizations.dart';
+import 'package:calories/utils/app_colors.dart';
 import 'package:calories/utils/app_router.dart';
+import 'package:calories/utils/app_spacing.dart';
 import 'package:calories/utils/error_details_notifier.dart';
+import 'package:calories/utils/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,11 +36,16 @@ void logUnhandledErrors(error, stack) {
   developer.log("UnhandledError", error: error, stackTrace: stack);
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final errorDetailsProvider = ref.watch(errorDetailsNotifierProvider);
+    final errorDetailsProviderNotifier = ref.watch(
+      errorDetailsNotifierProvider.notifier,
+    );
+
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Inter'),
       locale: Locale('en'),
@@ -50,6 +58,52 @@ class MainApp extends StatelessWidget {
       ],
       routes: AppRoutes.routesMap,
       initialRoute: AppRoutes.splash,
+      builder: (context, child) {
+        return Stack(
+          children: [
+            if (child != null) child,
+
+            if (errorDetailsProvider != null)
+              Positioned.fill(
+                child: SizedBox(
+                  width: context.deviceWt,
+                  height: context.deviceHt,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: AppColors.appBlack.withValues(alpha: 0.3),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: errorDetailsProvider.exception.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        AppSpacing.h20,
+                        TextButton(
+                          onPressed: () {
+                            errorDetailsProviderNotifier.errorResolved();
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.greenAccent,
+                          ),
+                          child: Text("Resolve"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
