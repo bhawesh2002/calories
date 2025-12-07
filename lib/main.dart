53a642ec5df134947e6d8eac990e9a1f4d40b831
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-12-07 09:39:20 +0530

    feat: add global error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
+import 'dart:async';
+
 import 'package:calories/data/services/storage_service.dart';
 import 'package:calories/l10n/app_localizations.dart';
 import 'package:calories/utils/app_router.dart';
+import 'package:calories/utils/error_details_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:developer' as developer;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await StorageService.setup();
-  runApp(ProviderScope(child: const MainApp()));
+  final ProviderContainer providerContainer = ProviderContainer();
+
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      FlutterError.onError = (details) {
+        providerContainer
+            .read(errorDetailsNotifierProvider.notifier)
+            .errorDetected(details);
+      };
+      await StorageService.setup();
+      runApp(ProviderScope(child: const MainApp()));
+    },
+    (error, stack) {
+      logUnhandledErrors(error, stack);
+    },
+  );
+}
+
+void logUnhandledErrors(error, stack) {
+  developer.log("UnhandledError", error: error, stackTrace: stack);
 }
 
 class MainApp extends StatelessWidget {
